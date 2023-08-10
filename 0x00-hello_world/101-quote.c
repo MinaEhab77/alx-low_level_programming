426336f1575e284b7070c7ba8110748c15d4c5fd
MinaEhab <fouadattia09@gmail.com>
2023-08-10 15:16:21 -0700

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char sentence("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char sentence [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(0, sentence, 58);
 	return (1);
 }
