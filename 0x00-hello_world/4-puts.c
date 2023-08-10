745d64460fa94ffd1c5546beb894de152591700b
MinaEhab <fouadattia09@gmail.com>
2023-08-10 14:07:45 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: C code using "puts" function to print a one statment
+ *
+ * Return: always 0 which means (Success)
+**/
+
+
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
