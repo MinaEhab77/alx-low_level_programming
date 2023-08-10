02fd8722547df1d388ef1bd3cdc6bc725d1f9275
MinaEhab <fouadattia09@gmail.com>
2023-08-10 14:33:51 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: C code using "puts" function to print a one statment
+ *
+ * Return: always 0 which means (Success)
+**/
+
+
+int main(void)
+{
+printf("Size of a char", sizeof(char));
+printf("Size of a int", sizeof(int));
+printf("Size of a long int", sizeof(long int));
+printf("Size of a long long int", sizeof(long long int));
+printf("size of a float", sizeof(float));
+return (0);
+}
