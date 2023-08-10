cc30f713f446d8029f62c0fdc3f8c83cb0239d13
MinaEhab <fouadattia09@gmail.com>
2023-08-10 15:27:10 -0700

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,15 @@
  *
  * Description: C code using "puts" function to print a one statment
  *
- * Return: always 1(error)
-**/
+ * Return: always 1(error).
+ *
+ * **/
 
 
 int main(void)
 {
 	char  sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,sentence,59);
+	write(1, sentence, 59);
 	return (1);
 }
