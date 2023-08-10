68914b870683c37dbd53e97ba3842abffd530584
MinaEhab <fouadattia09@gmail.com>
2023-08-10 14:19:35 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: C code using "puts" function to print a one statment
+ *
+ * Return: always 0 which means (Success)
+**/
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
