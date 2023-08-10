278ce5e271802d09ea62fb2d2002708c8de0bead
MinaEhab <fouadattia09@gmail.com>
2023-08-10 15:09:51 -0700

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main -Entry point
  *
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
+	char sentence("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(0,sentenc,58);
+	return (0);
 }
