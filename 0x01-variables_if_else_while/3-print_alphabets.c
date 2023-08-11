179c1eba4b3922c35dd37114aed62790b7ffe1f5
MinaEhab77 <fouadattia09@gmail.com>
2023-08-11 16:38:46 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char ch = 'a';
+        char ch = 'A';
+        while (ch <= 'z')
+        {
+                putchar(ch);
+                ch++;
+        }
+         while (ch <= 'z')
+        {
+                putchar(CH);
+                CH++;
+        }
+        putchar('\n');
+
+        return (0);
+}
