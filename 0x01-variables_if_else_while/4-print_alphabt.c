8d9ce3010b05e4340499136c81bf3b37541640b9
MinaEhab77 <fouadattia09@gmail.com>
2023-08-12 13:18:37 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        char ch = 'a';
+        while (ch <= 'z')
+        {
+            if (ch == 'e' || ch == 'q')
+                ch++;
+        putchar(ch );
+        ch++;
+        }
+    
+        return (0);
+}
