04dd049265dd180dd0339c4b5e4d034571d74282
MinaEhab77 <fouadattia09@gmail.com>
2023-08-12 13:32:52 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i+'0');
+i++;
+}
+return (0);
+}
+
