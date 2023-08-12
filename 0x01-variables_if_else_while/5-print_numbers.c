3022de70f7f0a547d6352fff325e70467772ee33
MinaEhab77 <fouadattia09@gmail.com>
2023-08-12 13:25:53 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%i", i);
+i++;
+}
+return (0);
+}
