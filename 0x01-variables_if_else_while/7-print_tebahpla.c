7c78163e76601f9502361a069d5920a078a47cda
MinaEhab77 <fouadattia09@gmail.com>
2023-08-12 13:45:20 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char CH = 'z';
+while (CH >= 'a')
+{
+putchar(CH);
+CH--;
+}
+return (0);
+}
