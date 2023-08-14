46883a87339e9340cc29fdcde0943f550183d74b
MinaEhab77 <fouadattia09@gmail.com>
2023-08-14 14:43:31 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0, k = 0, s;
+while (i < 10)
+{
+k = 0;
+while (k < 10)
+{
+s = 0;
+while (s < 10)
+{
+if (i != k && i < k && k != s && k < s)
+{
+putchar(i + '0');
+putchar(k + '0');
+putchar(s + '0');
+if (i + s + k != 24)
+{
+putchar(' ');
+putchar(',');
+}
+}
+s++;
+}
+k++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
