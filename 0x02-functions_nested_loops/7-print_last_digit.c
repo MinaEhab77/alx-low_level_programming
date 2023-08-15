5d9cbb24c74e6167f8621019a04e44029c4284a0
MinaEhab77 <fouadattia09@gmail.com>
2023-08-15 13:52:35 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - heb nadya w heb lolo
+ *
+ * @a: checks the input of function
+ *
+ * Return: returns 1 if 'c' is lowercase
+ *              otherwise always (0) success
+ */
+
+int print_last_digit(int a)
+{
+int digit;
+digit = a % 10;
+return (digit);
+}
