0b6e4db46cf2993702438b41caaf8b0e12b3284f
MinaEhab77 <fouadattia09@gmail.com>
2023-08-15 11:36:26 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+    for (int ch = 'a'; ch <= 'z'; ch++)
+         _putchar(ch);
+    _putchar('\n');
+}
