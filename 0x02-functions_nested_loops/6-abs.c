43fec3c257a5e5ad5dab1a4186b1349d469203ca
MinaEhab77 <fouadattia09@gmail.com>
2023-08-15 13:45:56 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_abs - heb nadya w heb lolo
+ *
+ * @a: checks the input of function
+ *
+ * Return: returns 1 if 'c' is lowercase
+ *              otherwise always (0) success
+ */
+
+int _abs(int a)
+{
+if (a < 0)
+{
+a = (a * -1);
+}
+return (a);
+}
