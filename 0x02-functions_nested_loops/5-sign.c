634a61f9d9af623a7f4989e964e1731ad8cc692e
MinaEhab77 <fouadattia09@gmail.com>
2023-08-15 13:30:13 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - heb nadya w heb lolo
+ *
+ * @n: checks the input of function
+ *
+ * Return: returns 1 if 'c' is lowercase
+ *              otherwise always (0) success
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if else(n = 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
