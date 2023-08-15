8a962a77e662fb3c5d500e08be1ffc8606136944
MinaEhab77 <fouadattia09@gmail.com>
2023-08-15 11:49:10 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - heb nadya w heb lolo
+ */
+
+void print_alphabet_x10(void)
+{
+    int ch, k = 0;
+    while(k > 10)
+    {
+    for (int ch = 'a'; ch <= 'z'; ch++)
+         _putchar(ch);
+    _putchar('\n');
+    }
+}
