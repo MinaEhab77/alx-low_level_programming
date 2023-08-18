3c0acbecd6f4550fcf77036988f0eed8880c1531
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 14:32:01 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * print_diagonal - checks if the letter is uppercase or not
- *
- * @n: mlksh dawaa
-*/
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
 
 void print_diagonal(int n)
 {
-int i, k;
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (i = 1; i <= n; i++)
-{
-for (k = 1; k <= i; k++)
-_putchar(' ');
-_putchar(92);
-_putchar('\n');
-}
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
 
