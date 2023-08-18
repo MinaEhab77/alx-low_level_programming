095e93f1953bb531f90921866f44d0f003b39e7e
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 08:27:30 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - checks if the letter is uppercase or not
+ *
+ * Return: 0 if not upper and 1 if upper
+*/
+void more_numbers(void)
+{
+int i, k, j;
+
+for (i = 0; i < 10; i++)
+{
+for (k = 0; k <= 14; k++)
+{
+j = k;
+if (k > 9)
+{
+putchar(1 + 48);
+j = k % 10;
+}
+putchar(j + 48);
+}
+putchar('\n');
+}
+}
