1f6fac3ece5051e25bc52deefce017a12c820884
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 06:09:58 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,16 +3,4 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
-int add(int, int);
-int _isupper(int c);
 #endif
