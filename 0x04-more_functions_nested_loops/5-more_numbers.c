78a53c5fa1c35267efbae071d2e55632350a68e3
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 08:32:37 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,22 +5,24 @@
  *
  * Return: 0 if not upper and 1 if upper
 */
+
 void more_numbers(void)
 {
-int i, k, j;
+	int i, k, j;
 
-for (i = 0; i < 10; i++)
-{
-for (k = 0; k <= 14; k++)
-{
-j = k;
-if (k > 9)
-{
-_putchar(1 + 48);
-j = k % 10;
-}
-_putchar(j + 48);
-}
-_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			j = k;
+			
+			if (k > 9)
+			{
+				_putchar(1 + 48);
+				j = k % 10;
+			}
+			_putchar(j + 48);
+		}
+	_putchar('\n');
+	}
 }
