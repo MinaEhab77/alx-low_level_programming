860891987e59b91f62ec533b2e8b3112d2ce0aab
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 07:38:59 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,8 +7,8 @@ void print_most_numbers(void)
 {
 int i;
 
-for (i = 48; i < 58; i++)
-if (i != 50 && i != 52)
+for (i = '0'; i < '9'; i++)
+if (i != '2' && i != '4')
 _putchar(i);
 _putchar('\n');
 }
