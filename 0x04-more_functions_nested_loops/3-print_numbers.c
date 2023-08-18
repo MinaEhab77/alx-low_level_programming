cb76cb72c9175df289b1cea59ca6fe0e364a4377
MinaEhab77 <fouadattia09@gmail.com>
2023-08-18 07:25:54 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,9 @@
  * Return: 0 if not upper and 1 if upper
 */
 
-void print_numbers (void)
+void print_numbers(void)
 {
-int i;
-
-for (i = 48; i < 58; i++)
+for (int i = 48; i < 58; i++)
 _putchar(i);
 _putchar('\n');
 }
