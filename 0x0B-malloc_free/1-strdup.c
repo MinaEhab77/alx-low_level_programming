514d0a365e29804757ee8ddaf05ba157637a1cb8
MinaEhab77 <fouadattia09@gmail.com>
2023-09-04 14:44:16 -0700

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+int size, i;
+char *s;  
+if (str == NULL)
+return(NULL);     
+for (size = 0; str[size] != '\0'; size++)
+;   
+s = malloc(size * sizeof(*str) + 1);
+if (s == NULL)
+return (NULL);
+else
+{
+for (i = 0; i < size; i++)
+s[i] = str[i];
+}
+return (s);
+}
