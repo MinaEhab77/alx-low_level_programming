bd3af95e194add936dd727160041e595b1ccc51c
MinaEhab77 <fouadattia09@gmail.com>
2023-09-04 15:10:36 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,17 +10,18 @@ char *_strdup(char *str)
 {
 int size, i;
 char *s;
+
 if (str == NULL)
-return (NULL);
+	return (NULL);
 for (size = 0; str[size] != '\0'; size++)
 ;
 s = malloc(size * sizeof(*str) + 1);
 if (s == NULL)
-return (NULL);
+	return (NULL);
 else
 {
-for (i = 0; i < size; i++)
-s[i] = str[i];
+	for (i = 0; i < size; i++)
+	s[i] = str[i];
 }
 return (s);
 }
