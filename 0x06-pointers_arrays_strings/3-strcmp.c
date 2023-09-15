d61208ca66d124e53817d8f20d1aef2b590c7ad2
MinaEhab77 <fouadattia09@gmail.com>
2023-09-15 12:11:10 -0700

    mina ehab

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,19 +6,16 @@
  *
  * Return: s1[i] - s2[i]
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(const char* s1, const char* s2)
 {
-	int i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
-	}
-	return (0);
+while (*s1 != '\0' || *s2 != '\0')
+{
+if (*s1 < *s2)
+return -1;
+else if (*s1 > *s2)
+return 1;
+s1++;
+s2++;
 }
-
+return (0);
+}
