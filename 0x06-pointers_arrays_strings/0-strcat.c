9d7f1600c93f3fcabfaca851021fcff88289e698
MinaEhab77 <fouadattia09@gmail.com>
2023-09-15 10:05:35 -0700

    mina ehab

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,9 +6,9 @@
  *
  * Return: void
  */
-char* _strcat(char* dest, const char* src)
+char *_strcat(char* dest, const char* src)
 {
-char* dest_ptr = dest;
+char *dest_ptr = dest;
 while (*dest_ptr != '\0')
 dest_ptr++;
 while (*src != '\0')
