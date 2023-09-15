07d6e783526fc550c705aaabba741ade391cc2fe
MinaEhab77 <fouadattia09@gmail.com>
2023-09-15 14:53:54 -0700

    mina ehab

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,25 +4,22 @@
  * @s: input value
  * Return: pointer
  */
-char *leet(char *s)
+char *leet(char *n)
 {
-char *s_ptr = s;
-char s1[] = "aAeEoOtTlL";
-char s2[] = "4433007711";
-int i = 0;
-while (*s != '\0')
-{
-int j = 0;
-while (j < 10)
-{
-if (s[i] == s1[j])
-{
-s[i] = s2[j];
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
 }
-j++;
-}
-i++;
-s++;
-}
-return (s_ptr);
-}
+
