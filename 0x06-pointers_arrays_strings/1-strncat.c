b98aaa33f4342b08930c65a862dcded280679447
MinaEhab77 <fouadattia09@gmail.com>
2023-09-15 10:33:20 -0700

    mina ehab

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+char* dest_ptr = dest;
+while (*dest_ptr != '\0')
+dest_ptr++;
+while (*src != '\0' && n > 0)
+{
+*dest_ptr = *src;
+dest_ptr++;
+src++;
+n--;
 }
-
+*dest_ptr = '\0';
+return dest;
+}{
