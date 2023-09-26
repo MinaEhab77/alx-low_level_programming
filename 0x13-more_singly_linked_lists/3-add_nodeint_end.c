3b01329d4bf5cf7b5f1557beccfad1e24fba5ad5
MinaEhab77 <fouadattia09@gmail.com>
2023-09-26 14:53:23 -0700

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = NULL;
-	
+		
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new_node;
