ec9d699f3d53368f329c5dbaba75967e9e95bbaa
MinaEhab77 <fouadattia09@gmail.com>
2023-09-26 07:05:58 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,26 +3,29 @@
 #include "lists.h"
 
 /**
- * add_node - adds new kofta beginning of a linked list
- * @head: the list_t list
- * @str: estring to add in node
- * Return: address of the new elem
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to a pointer to the head node of the list
+ * @str: string to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL if it failed
  */
-
-
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-unsigned int len = 0;
+list_t *new_node;
 
-while (str[len])
-len++;
-new = malloc(sizeof(list_t));
-if (!new)
+if (head == NULL || str == NULL)
 return (NULL);
-new->str = strdup(str);
-new->len = len;
-new->next = (*head);
-(*head) = new;
-return (*head);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = strlen(str);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
