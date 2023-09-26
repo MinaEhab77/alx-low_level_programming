e4fb8bdf5dcddcb5852f58ca89c7a7eb0a7edaee
MinaEhab77 <fouadattia09@gmail.com>
2023-09-26 12:31:21 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+/**
+ * print_meassage - prints a sentence before the main
+ * function is executed
+ */
+
 void print_message(void)
 {
-printf("You're beat! and yet, you must allow,\n" "I bore my house upon my back!\n");
-}
-
-int main(void)
-{
-print_message();
-return 0;
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
 }
