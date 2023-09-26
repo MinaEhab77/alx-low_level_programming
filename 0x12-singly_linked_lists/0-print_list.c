0c3e08fecdb392e9ca16187d6f5563690957ec12
MinaEhab77 <fouadattia09@gmail.com>
2023-09-26 06:42:45 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,10 @@ size_t count = 0;
 
 while (h != NULL)
 {
-printf("[%d] ", h->len);
 if (h->str == NULL)
 printf("[0] (nil)\n");
 else
-printf("%s\n", h->str);
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 count++;
 }
