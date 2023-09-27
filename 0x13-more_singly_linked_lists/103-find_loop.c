b782575a9c5b64ae92088d8d103ea7fc3b334e4d
MinaEhab77 <fouadattia09@gmail.com>
2023-09-26 17:48:34 -0700

    updated

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,41 +1,35 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
  *
- * Return: 1 (Success), or -1 (Fail)
+ * Return: address of the node where the loop starts, or NULL
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	if (*head == NULL)
-		return (-1);
+	if (!head)
+		return (NULL);
 
-	if (index == 0)
+	while (slow && fast && fast->next)
 	{
-		*head = (*head)->next;
-		free(temp);
-		return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
 	}
 
-	while (i < index - 1)
-	{
-		if (!temp || !(temp->next))
-			return (-1);
-		temp = temp->next;
-		i++;
-	}
-
-
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-
-	return (1);
+	return (NULL);
 }
 
